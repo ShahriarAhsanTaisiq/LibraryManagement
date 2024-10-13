a5a4e3827ef6d354d363bbc8c6f801cfba88039f
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-10-13 23:09:08 +0600

    Add Custom query method for "findByLibraryMemberId"

--- src/main/java/com/springBoot/libraryManagement/repository/LoanRecordRepository.java
+++ src/main/java/com/springBoot/libraryManagement/repository/LoanRecordRepository.java
@@ -2,7 +2,11 @@ package com.springBoot.libraryManagement.repository;
 
 import com.springBoot.libraryManagement.model.LoanRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface LoanRecordRepository extends JpaRepository<LoanRecord, Long> {
-
+    List<LoanRecord> findByLibraryMemberId(Long memberId);
 }
