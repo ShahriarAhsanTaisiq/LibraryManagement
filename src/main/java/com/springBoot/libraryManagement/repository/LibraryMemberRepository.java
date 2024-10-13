3a5b848736a619f6232aa86c13152f8596c98e81
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-10-13 23:08:22 +0600

    Change a silly issue.

--- src/main/java/com/springBoot/libraryManagement/repository/LibraryMemberRepository.java
+++ src/main/java/com/springBoot/libraryManagement/repository/LibraryMemberRepository.java
@@ -2,7 +2,9 @@ package com.springBoot.libraryManagement.repository;
 
 import com.springBoot.libraryManagement.model.LibraryMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibraryMemberRepository extends JpaRepository<LibraryMember, Long> {
 
 }
--- src/main/java/com/springBoot/libraryManagement/repository/PublisherRepository.java
+++ src/main/java/com/springBoot/libraryManagement/repository/PublisherRepository.java
@@ -2,7 +2,9 @@ package com.springBoot.libraryManagement.repository;
 
 import com.springBoot.libraryManagement.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
 
 }
