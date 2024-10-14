8a89c398b281b8a80826b79b4aaff580b8558678
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-10-14 14:28:42 +0600

    Add lombok annotation in the entity files.

--- src/main/java/com/springBoot/libraryManagement/model/Publisher.java
+++ src/main/java/com/springBoot/libraryManagement/model/Publisher.java
@@ -1,11 +1,19 @@
 package com.springBoot.libraryManagement.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.boot.model.source.spi.EmbeddedAttributeMapping;
 
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
