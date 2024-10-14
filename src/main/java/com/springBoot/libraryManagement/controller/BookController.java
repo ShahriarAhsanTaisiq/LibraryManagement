5f94a105ca3b80da25225385dd21a12759e3288e
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-10-14 18:29:43 +0600

    Resolve a small issue.

--- src/main/java/com/springBoot/libraryManagement/controller/BookController.java
+++ src/main/java/com/springBoot/libraryManagement/controller/BookController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public class BookController {
 
     @Autowired
-    private final BookeService bookeService;
+    private  BookeService bookeService;
 
     public BookController(BookeService bookeService) {
         this.bookeService = bookeService;
